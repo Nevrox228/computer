4ea0b856c99ba8f1f8a0c0b2c9b015373e270748
Nikita <tkacnikita716@gmail.com>
2021-06-16 20:56:22 +0200

    start animate

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,3 @@
-// const hamburger = document.querySelector('.hamburger'),
-//       menu = document.querySelector('.menu'),
-//       closeElem = document.querySelector('.menu__close');
-
-// hamburger.addEventListener('click', () => {
-//     menu.classList.add('active');
-// });
-
-// closeElem.addEventListener('click', () => {
-//     menu.classList.remove('active');
-// });
+$(document).ready(function(){
+    new WOW().init();
+});
